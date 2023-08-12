4d60aaf33a071f56229142c3dc0db40cb58f2a7c
RadwanHegazy <radwangaber22@gmail.com>
2023-08-12 15:12:08 +0300

    generate auto tokens for each user

--- app/models.py
+++ app/models.py
@@ -1,9 +1,18 @@
 from django.db import models
-# Create your models here.
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
 class NameModel (models.Model) :
     name = models.CharField(max_length=100)
 
     def __str__(self) : 
         return f'{self.name}'
+
+
+
+@receiver(post_save,sender=User)
+def CreateTokenForUser (created,instance,**args) : 
+    if created : 
+        Token.objects.create(user = instance)
